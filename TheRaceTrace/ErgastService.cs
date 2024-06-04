06e87fed7049a600cade56130a2c125656ab38b8
Ibraheem Sajid <ibraheem.sajid@gmail.com>
2024-06-04 05:32:19 +0100

    add in oxyplot

--- TheRaceTrace/ErgastService.cs
+++ TheRaceTrace/ErgastService.cs
@@ -13,19 +13,17 @@ namespace TheRaceTrace
     public class ErgastService
     {
         // CA1822 will go when I add interfaces apparently
-        //public Dictionary<int, List<LapTime>> GetLapTimes()
-        public void GetLapTimes()
+        public Dictionary<int, LapTime[]> GetLapTimes()
         {
             Dictionary<int, LapTime[]> lapTimesByLap = [];
-            using (StreamReader r = new("06Monaco19.json"))
+            string file = "06Monaco19.json";
+            string json = File.ReadAllText(file);
+            dynamic? data = JsonConvert.DeserializeObject(json);
+            foreach (var lap in data!.MRData.RaceTable.Races[0].Laps)
             {
-                string json = r.ReadToEnd();
-                dynamic? data = JsonConvert.DeserializeObject(json);
-                foreach (var lap in data.MRData.RaceTable.Races[0].Laps)
-                {
-                    lapTimesByLap[lap.Value<int>("number")] = lap.Timings.ToObject<LapTime[]>();
-                }
+                lapTimesByLap[lap.Value<int>("number")] = lap.Timings.ToObject<LapTime[]>();
             }
+            return lapTimesByLap;
         }
     }
 
--- TheRaceTrace/ViewModel.cs
+++ TheRaceTrace/ViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OxyPlot;
+using OxyPlot.Series;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +14,51 @@ namespace TheRaceTrace
         private readonly DelegateCommand _getLapTimesCommand;
         private readonly ErgastService _ergastService;
 
+        private Dictionary<int, LapTime[]>? _lapTimesByLap;
+
         public ViewModel(ErgastService ergastService)
         {
             _getLapTimesCommand = new DelegateCommand(OnGetLapTimes);
             _ergastService = ergastService;
+
+            Plot = new PlotModel
+            {
+                Series =
+                {
+                    CreateSeries("Series 1"),
+                    CreateSeries("Series 2"),
+                    CreateSeries("Series 3")
+                }
+            };
         }
 
         public ICommand GetLapTimesCommand => _getLapTimesCommand;
 
+        public Dictionary<int, LapTime[]>? LapTimesByLap
+        {
+            get => _lapTimesByLap;
+            set => SetProperty(ref _lapTimesByLap, value);
+        }
+
+        public PlotModel Plot { get; }
+
+        private static LineSeries CreateSeries(string name)
+        {
+            return new LineSeries
+            {
+                Title = name,
+                Points =
+                {
+                    new(0, 0),
+                    new(1, 1),
+                    new(2, 2)
+                }
+            };
+        }
+
         private void OnGetLapTimes(object? commandParameter)
         {
-            _ergastService.GetLapTimes();
+            LapTimesByLap = _ergastService.GetLapTimes();
         }
 
     }
